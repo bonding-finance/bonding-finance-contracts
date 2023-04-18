8fd60cfa49535853ab98b7aeaef25406735434b5
bonding-finance-dev <bondingfinance@proton.me>
2023-04-18 00:21:09 -0500

    Add missing natspec

--- contracts/PerpetualBond/PerpetualBondDeployer.sol
+++ contracts/PerpetualBond/PerpetualBondDeployer.sol
@@ -5,9 +5,18 @@ pragma solidity ^0.8.0;
 import "./interfaces/IPerpetualBondDeployer.sol";
 import "./PerpetualBondVault.sol";
 
+/**
+ * @title Bonding Finance bond deployer
+ * @author Bonding Finance
+ */
 contract PerpetualBondDeployer is IPerpetualBondDeployer {
     Parameters public override parameters;
 
+    /**
+     * @notice Deploys bond contract
+     * @param factory Factory address
+     * @param token Underlying token of the bond
+     */
     function deploy(address factory, address token) internal returns (address bond) {
         parameters = Parameters({factory: factory, token: token});
         bond = address(new PerpetualBondVault{salt: keccak256(abi.encode(token))}());
